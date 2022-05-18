a14d023cc9e81be5a571e0c8aa3888b95ee0c4ba
David <david.eriksson@valtech.com>
2022-05-18 10:05:22 +0200

    feat(controller): add RedirectUser controller

--- lib/controllers.go
+++ lib/controllers.go
@@ -1,1 +1,26 @@
 package lib
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+/*
+  Controller:
+  - RedirectUser: redirects request to SPOTIFY_AUTH_URL
+  
+  @function Parameters:
+  - w: http.ResponseWriter - response writer
+  - r: *http.Request - request
+
+  @function Returns:
+  - nil
+*/
+func RedirectUser(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  redirUri, _ := r.URL.Parse("http://" + os.Getenv("GOOSE_SERVER_HOST") + ":" + os.Getenv("GOOSE_SERVER_PORT") + "/callback")
+  query := BuildQueryParams(r, redirUri)
+  fmt.Printf("Redirecting to %s\n", redirUri)
+  http.Redirect(w, r, os.Getenv("SPOTIFY_AUTH_URL") + query, http.StatusTemporaryRedirect)
+}
